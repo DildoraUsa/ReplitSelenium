f88be05afdc9a72b96b1671986a8bcef443ba122
Dildora Sodiqkhojieva <ominaxon21@gmail.com>
2022-06-12 16:57:55 -0500

    Script2 works Script1 it is note page

--- src/Script.java
+++ src/Script.java
@@ -10,60 +10,60 @@ import java.util.concurrent.TimeUnit;
 
 public class Script {
     public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\omina\\Downloads\\chromedriver_win32\\chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
-        driver.get("http://qa-duotify.us-east-2.elasticbeanstalk.com/register.php");
-         //Thread.sleep(1000);
-        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
-        String expectedTitle = "Welcome to Duotify!";
-        String actualTitle = driver.getTitle();
-       if (actualTitle.equals(expectedTitle)){
-           System.out.println("Correct Title");
-       }else {
-           System.out.println("Title not match");
-       }
-       driver.findElement(By.id("hideLogin")).click();
-        WebElement username = driver.findElement(By.name("username"));                  //By.id("\"username\""));
-        WebElement firstName =driver.findElement(By.id("firstName"));
-        WebElement lastName =driver.findElement(By.id("lastName"));
-        WebElement email =driver.findElement(By.id("email"));
-        WebElement confirmEmail =driver.findElement(By.id("email2"));
-        WebElement password =driver.findElement(By.id("password"));
-        WebElement confirmPassword =driver.findElement(By.id("password2"));
-       // Thread.sleep(1000);
-        String nameUser = randomName();
-        username.sendKeys(nameUser);
-        //Thread.sleep(1000);
-        String givenName = randomName();
-       firstName.sendKeys(givenName);
-       String sureName = "Olimjonova";
-        lastName.sendKeys(sureName);
-        String randomEmail = randomEmail2();
-        email.sendKeys(randomEmail);
-        confirmEmail.sendKeys(randomEmail);
-        String randomPassword = "Dildora123";
-       password.sendKeys(randomPassword);
-       confirmPassword.sendKeys(randomPassword+ Keys.ENTER);
-       String actualUrl = driver.getCurrentUrl();
-       String expectedUrl = "http://qa-duotify.us-east-2.elasticbeanstalk.com/browse.php?";
-
-       if (actualUrl.equals(expectedUrl)){
-           System.out.println("Correct Url");
-       }else {
-           System.out.println("Wrong Url");
-       }
-        String matches = driver.findElement(By.id("nameFirstAndLast")).getText();
-       String matches1 = givenName +" "+sureName;
-       if (matches.equals(matches1)){
-           System.out.println("First name and Last name match");
-       }else {
-           System.out.println("First name and Last name doesn't match");
-       }
-       //Thread.sleep(3000);
-        driver.findElement(By.id("nameFirstAndLast")).click();
-        String actualUserInfo = driver.findElement(By.tagName("h1")).getText();
-       /// Thread.sleep(1000);
-        Assert.assertEquals(actualUserInfo,matches1);
+//        System.setProperty("webdriver.chrome.driver","C:\\Users\\omina\\Downloads\\chromedriver_win32\\chromedriver.exe");
+//        WebDriver driver= new ChromeDriver();
+//        driver.get("http://qa-duotify.us-east-2.elasticbeanstalk.com/register.php");
+//         //Thread.sleep(1000);
+//        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+//        String expectedTitle = "Welcome to Duotify!";
+//        String actualTitle = driver.getTitle();
+//       if (actualTitle.equals(expectedTitle)){
+//           System.out.println("Correct Title");
+//       }else {
+//           System.out.println("Title not match");
+//       }
+//       driver.findElement(By.id("hideLogin")).click();
+//        WebElement username = driver.findElement(By.name("username"));                  //By.id("\"username\""));
+//        WebElement firstName =driver.findElement(By.id("firstName"));
+//        WebElement lastName =driver.findElement(By.id("lastName"));
+//        WebElement email =driver.findElement(By.id("email"));
+//        WebElement confirmEmail =driver.findElement(By.id("email2"));
+//        WebElement password =driver.findElement(By.id("password"));
+//        WebElement confirmPassword =driver.findElement(By.id("password2"));
+//       // Thread.sleep(1000);
+//        String nameUser = randomName();
+//        username.sendKeys(nameUser);
+//        //Thread.sleep(1000);
+//        String givenName = randomName();
+//       firstName.sendKeys(givenName);
+//       String sureName = "Olimjonova";
+//        lastName.sendKeys(sureName);
+//        String randomEmail = randomEmail2();
+//        email.sendKeys(randomEmail);
+//        confirmEmail.sendKeys(randomEmail);
+//        String randomPassword = "Dildora123";
+//       password.sendKeys(randomPassword);
+//       confirmPassword.sendKeys(randomPassword+ Keys.ENTER);
+//       String actualUrl = driver.getCurrentUrl();
+//       String expectedUrl = "http://qa-duotify.us-east-2.elasticbeanstalk.com/browse.php?";
+//
+//       if (actualUrl.equals(expectedUrl)){
+//           System.out.println("Correct Url");
+//       }else {
+//           System.out.println("Wrong Url");
+//       }
+//        String matches = driver.findElement(By.id("nameFirstAndLast")).getText();
+//       String matches1 = givenName +" "+sureName;
+//       if (matches.equals(matches1)){
+//           System.out.println("First name and Last name match");
+//       }else {
+//           System.out.println("First name and Last name doesn't match");
+//       }
+//       //Thread.sleep(3000);
+//        driver.findElement(By.id("nameFirstAndLast")).click();
+//        String actualUserInfo = driver.findElement(By.tagName("h1")).getText();
+//       /// Thread.sleep(1000);
+//        Assert.assertEquals(actualUserInfo,matches1);
 //       if (actualUserInfo.equals(matches1)){
 //           System.out.println("It is correct");
 //       }else {
@@ -71,7 +71,7 @@ public class Script {
 //       }
      // Thread.sleep(3000);
        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(1000));
-        driver.findElement(By.id("rafael")).click();
+       // driver.findElement(By.id("rafael")).click();
        // Thread.sleep(4000);
        // driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4000));
 //       String actualUrl2 = driver.getCurrentUrl();
